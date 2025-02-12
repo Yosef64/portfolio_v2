295661f77c2c53d676f3b23d00ddbe3123de427e
Yosef64 <yosefale65@gmail.com>
2025-02-12 09:21:05 +0300

    removing warnings

--- src/services/EmailServices.tsx
+++ src/services/EmailServices.tsx
@@ -1,27 +1,28 @@
 // import { Resend } from "resend";
-// // import EmailTemplate from "../components/EmailTemplate"; // Import your component
+// import EmailTemplate from "../components/EmailTemplate"; // Import your component
 
 // const resend = new Resend("re_9rsGC2PE_Jmqr6TWtJwjZTHWPyDnrGTNm");
 
-// export async function sendEmail(
-//   name: string,
-//   email: string,
-//   subject: string,
-//   message: string
-// ): Promise<void> {
+export async function sendEmail(
+  name: string,
+  email: string,
+  subject: string,
+  message: string
+): Promise<void> {
   
 
-//   try {
-//     await fetch("http://localhost:5000/send-email", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         to: email,
-//         subject: subject,
-//         text: message,
-//       }),
-//     });
-//   } catch (err) {
-//     console.log("Error sending email", err);
-//   }
-// }
+  try {
+    await fetch("http://localhost:5000/send-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        to: email,
+        subject: subject,
+        text: message,
+      }),
+    });
+    console.log(name);
+  } catch (err) {
+    console.log("Error sending email", err);
+  }
+}
